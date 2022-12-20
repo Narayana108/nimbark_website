7e60a6f8f9145358e714dce28cf5e19f1bebbb9c
Narayana108 <narayan-das@tuta.io>
2022-12-20 21:32:18 +0530

    Site structure
    Signed-off-by: Narayana108 <narayan-das@tuta.io>

--- postcss.config.js
+++ postcss.config.js
@@ -3,4 +3,13 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
+  theme: {
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
+    }
+  }
 };
